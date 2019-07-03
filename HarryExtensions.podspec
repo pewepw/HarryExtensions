d16e2ed93b11f8cf72360c92e340491d92491d6d
Harry <poripori1030@gmail.com>
2019-07-03 21:09:11 +0800

    0.2.6

--- HarryExtensions.podspec
+++ HarryExtensions.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HarryExtensions'
-  s.version          = '0.2.5'
+  s.version          = '0.2.6'
   s.swift_version    = '4.2'
   s.summary          = 'All the good stuff.'
 
--- HarryExtensions/Classes/Extension+Button.swift
+++ HarryExtensions/Classes/Extension+Button.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIButton {
-    func leftImage(image: UIImage, renderMode: UIImage.RenderingMode) {
+    public func leftImage(image: UIImage, renderMode: UIImage.RenderingMode) {
         self.setImage(image.withRenderingMode(renderMode), for: .normal)
         self.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: image.size.width)
         self.imageView?.contentMode = .scaleAspectFit
